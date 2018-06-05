56862578179619f0cfd05c5920a12eb33ba4f6b8
alexencinas <shoogiee123@gmail.com>
2018-06-05 23:02:16 +0000

    Done.

--- currency.rb
+++ currency.rb
@@ -1,1 +1,57 @@
-# Write your cod here
+# Write your code here
+def usd_to_eur 
+  dollars = gets.chomp.to_i
+  euros = (dollars)*0.85
+  puts euros
+end
+usd_to_eur
+
+def eur_to_usd
+  euros = gets.chomp.to_i
+  dollars = (euros)*1.17
+  puts dollars
+end
+eur_to_usd
+
+def usd_to_yen
+  dollars = gets.chomp.to_i
+  yen = (dollars)*109.83
+  puts yen
+end
+usd_to_yen
+
+def usd_to_gbp
+  dollars = gets.chomp.to_i
+  pounds = (dollars)*0.75
+  puts pounds
+end
+usd_to_gbp
+
+def usd_to_aud
+  dollars = gets.chomp.to_i
+  aud = (dollars)*1.31
+  puts aud
+end
+usd_to_aud
+
+def jpy_to_usd
+  jpy = gets.chomp.to_i
+  dollars = (jpy)*0.0091
+  puts dollars
+end 
+jpy_to_usd
+
+def gbp_to_usd
+  gbp = gets.chomp.to_i
+  dollars = (gbp)*1.34
+  puts dollars
+end 
+gbp_to_usd
+
+def aud_to_usd
+  aud = gets.chomp.to_i
+  dollars = (aud)*0.76
+  puts dollars
+end
+aud_to_usd
+  
