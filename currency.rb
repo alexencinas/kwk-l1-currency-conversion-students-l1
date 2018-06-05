b4f79faa1c3b21046659285077e4495151faa94c
alexencinas <shoogiee123@gmail.com>
2018-06-05 22:38:10 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,12 +1,12 @@
 # Write your code here
 def usd_to_eur 
   dollars = gets.chomp
-  euros = #{dollars}*0.85
+  euros = (dollars)*0.85
 end
 usd_to_eur
 
 def eur_to_usd
   euros = gets.chomp
-  dollars = #{euros}*1.17
+  dollars = (euros)*1.17
 end
 eur_to_usd
